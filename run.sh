234aae59b9185c1a2b746a4b45ecf9ec6986edc3
Chuppa <steve@chuppa.com.au>
2020-01-22 03:23:03 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-yum upgrade -y
-yum clean all
 rm /JSIT/justseedit_piece_downloader.jar -f
 rm /Logs/JSIT.log -f
 wget -q https://justseed.it/justseedit_piece_downloader.jar -O /JSIT/justseedit_piece_downloader.jar
